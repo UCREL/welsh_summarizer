1a560c36c96e4458b2244f87a5f35c5e55205ea9
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-30 13:29:57 +0100

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -85,8 +85,7 @@ def run_summarizer():
         st.markdown("#### Rhowch eich testun isod:")
         option = st.sidebar.radio('Sut ydych chi am fewnbynnu eich testun?', ('Defnyddiwch destun enghreifftiol', 'Rhowch eich testun eich hun', 'Llwythwch ffeil testun i fyny'))
         if option == 'Defnyddiwch destun enghreifftiol':
-           example_fname = st.sidebar.selectbox('Select example text:', 
-                    sorted([f for f in os.listdir(EXAMPLES_DIR) if f.startswith(('cy','ex'))]))
+           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir(EXAMPLES_DIR) if f.startswith(('cy','ex'))]))
 
            with open(os.path.join(EXAMPLES_DIR, example_fname), 'r', encoding='utf8') as example_file:
                example_text = example_file.read()
@@ -116,8 +115,7 @@ def run_summarizer():
         st.markdown("#### Enter your text below:")
         option = st.sidebar.radio('How do you want to input your text?', ('Use an example text', 'Paste a copied', 'Upload a text file'))
         if option == 'Use an example text':           
-           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir(EXAMPLES_DIR)
-                                                  if f.startswith(('en','ex'))]))
+           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir(EXAMPLES_DIR) if f.startswith(('en','ex'))]))
            with open(os.path.join(EXAMPLES_DIR, example_fname), 'r', encoding='utf8') as example_file:
                example_text = example_file.read()
                input_text = st.text_area('Summarise the example text in the box:', example_text, height=300)

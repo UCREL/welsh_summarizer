e4c3ba69011f8e6e70ecc025c8e7939907d21694
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-30 11:37:50 +0100

    update app.py and setup.py

--- app/app.py
+++ app/app.py
@@ -12,57 +12,61 @@ st.set_page_config(
      }
  )
 
-language = st.sidebar.selectbox('Newid iaith (Change language):', ['Cymraeg', 'English'])
-if language=='Cymraeg':
-     st.header('🌷 Croeso i’r Adnodd Creu Crynodebau (ACC) f.1.0')
-     st.subheader("Rhowch eich testun isod:")
-     
-     option = st.radio(
-          'Sut ydych chi am fewnbynnu eich testun?',
-          ('Defnyddiwch destun enghreifftiol', 'Rhowch eich testun eich hun'))
+# st.sidebar.markdown('# 🌼 Welsh FreeTxt')
 
-     chosen_ratio = st.sidebar.slider('Dewiswch gymhareb y crynodeb [10% i 50%]:',
-                min_value=10, max_value=50, step=10)/100
+run_summarizer()
+
+# language = st.sidebar.selectbox('Newid iaith (Change language):', ['Cymraeg', 'English'])
+# if language=='Cymraeg':
+     # st.header('🌷 Croeso i’r Adnodd Creu Crynodebau (ACC) f.1.0')
+     # st.subheader("Rhowch eich testun isod:")
+     
+     # option = st.radio(
+          # 'Sut ydych chi am fewnbynnu eich testun?',
+          # ('Defnyddiwch destun enghreifftiol', 'Rhowch eich testun eich hun'))
+
+     # chosen_ratio = st.sidebar.slider('Dewiswch gymhareb y crynodeb [10% i 50%]:',
+                # min_value=10, max_value=50, step=10)/100
                 
 
-     if option == 'Defnyddiwch destun enghreifftiol':
-          input_text = st.text_area('Crynhowch y testun enghreifftiol yn y blwch:', example_text, height=400)
-     else:
-          input_text = st.text_area('Teipiwch neu gludwch eich testun yn y blwch testun', '<Rhowch eich testun...>')
+     # if option == 'Defnyddiwch destun enghreifftiol':
+          # input_text = st.text_area('Crynhowch y testun enghreifftiol yn y blwch:', example_text, height=400)
+     # else:
+          # input_text = st.text_area('Teipiwch neu gludwch eich testun yn y blwch testun', '<Rhowch eich testun...>')
 
-     if st.button("Crynhoi👈"):
-       if input_text and input_text!='<Rhowch eich testun (Please enter your text...)>':
-         summary = text_rank_summarize(input_text, ratio=chosen_ratio)
-         if summary:
-            st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
-         else:
-            st.write(sent_tokenize(text_rank_summarize(input_text, ratio=0.5))[0])
-       else:
-         st.write("Rhowch eich testun...(Please enter your text...)")
+     # if st.button("Crynhoi👈"):
+       # if input_text and input_text!='<Rhowch eich testun (Please enter your text...)>':
+         # summary = text_rank_summarize(input_text, ratio=chosen_ratio)
+         # if summary:
+            # st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
+         # else:
+            # st.write(sent_tokenize(text_rank_summarize(input_text, ratio=0.5))[0])
+       # else:
+         # st.write("Rhowch eich testun...(Please enter your text...)")
                
-else:
-     st.header('🌷 Welcome to Welsh Text Summary Creator (ACC) v.1.0')
-     st.subheader('Enter your text below:')
+# else:
+     # st.header('🌷 Welcome to Welsh Text Summary Creator (ACC) v.1.0')
+     # st.subheader('Enter your text below:')
      
-     option = st.radio(
-          'How do you want to input your text?',
-          ('Use example text', 'Enter your own text'))
+     # option = st.radio(
+          # 'How do you want to input your text?',
+          # ('Use example text', 'Enter your own text'))
 
-     chosen_ratio = st.sidebar.slider('Select summary ratio [10% to 50%]',
-                min_value=10, max_value=50, step=10)/100
+     # chosen_ratio = st.sidebar.slider('Select summary ratio [10% to 50%]',
+                # min_value=10, max_value=50, step=10)/100
 
-     if option == 'Use example text':
-          input_text = st.text_area('Summarise the example text in the box:', example_text, height=400)
-     else:
-          input_text = st.text_area('Type or paste your text into the text box:', '<Please enter your text...>')
+     # if option == 'Use example text':
+          # input_text = st.text_area('Summarise the example text in the box:', example_text, height=400)
+     # else:
+          # input_text = st.text_area('Type or paste your text into the text box:', '<Please enter your text...>')
                
-     if st.button("Summarise👈"):
-       if input_text and input_text!='<Please enter your text...>':
-         summary = text_rank_summarize(input_text, ratio=chosen_ratio)
-         if summary:
-            st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
-         else:
-            st.write(sent_tokenize(text_rank_summarize(input_text, ratio=1))[0])
-         # process what needs to be displayed with regards to ratio
-       else:
-         st.write('Please enter your text')
+     # if st.button("Summarise👈"):
+       # if input_text and input_text!='<Please enter your text...>':
+         # summary = text_rank_summarize(input_text, ratio=chosen_ratio)
+         # if summary:
+            # st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
+         # else:
+            # st.write(sent_tokenize(text_rank_summarize(input_text, ratio=1))[0])
+         # # process what needs to be displayed with regards to ratio
+       # else:
+         # st.write('Please enter your text')
